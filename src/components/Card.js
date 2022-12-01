c4b5dbb3a7cc1d462125feb537a089aea04bffbf
Koray Guner <okorayguner@gmail.com>
2022-12-01 22:03:44 +0300

    fix: object-fit cover

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,7 +6,7 @@ function Card( {item: {char_id, name, img}}) {
       <Link to={`character/${char_id}`}>
     <div className="max-h-full rounded-lg shadow-md bg-zinc-800 dark:border-gray-700 transition-all opacity-80 hover:opacity-100">
         <div className="h-[260px] lg:h-[300px] xl:h-[360px] overflow-hidden p-2 " >
-        <img className="object-cover rounded-lg"  src={img} alt={name} />
+        <img className="w-full h-full object-cover rounded-lg"  src={img} alt={name} />
         </div>
         <div className="max-h-60 text-center py-2 text-amber-200 font-medium">{name}</div>
     </div>
